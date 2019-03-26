be5c9751497bb2fcc902cd6f30302dc2718bbb72
Stephanie <stephani.kell@gmail.com>
2019-03-26 10:40:11 -0400

    Complete setup and configuration

---
+++ frontend/pintrigue.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    // const store
+    ReactDOM.render(<h1>Welcome to Pintrigue</h1>, root);
+})
