f95ad12376ffe3efd758ec8037bc2afe05afe3da
Stephanie <stephani.kell@gmail.com>
2019-03-27 22:04:31 -0400

    Begi style Users Profile

--- frontend/components/user_show/user_profile.jsx
+++ frontend/components/user_show/user_profile.jsx
@@ -4,22 +4,70 @@ import { Route, Redirect, Switch, Link, HashRouter, withRouter } from 'react-rou
 class UserProfile extends React.Component {
     constructor(props) {
         super(props);
+        this.displayName = this.displayName.bind(this);
+        this.displayDescription = this.displayDescription.bind(this);
     }
 
     componentDidMount() {
         this.props.fetchUser(this.props.username);
     }
 
+    displayName() {
+        const { user } = this.props;
+        if (user.first_name || user.last_name ) {
+            return [user.first_name, user.last_name].join(" ");
+        } else if (user.first_name) {
+            return user.first_name;
+        } else if (user.last_name) {
+            return user.last_name;
+        } else {
+            return user.username;
+        }
+    }
+
+    displayDescription() {
+        const { user } = this.props;
+        if (user.location || user.description) {
+            return [user.location, user.description].join(" * ");
+        } else if (user.location) {
+            return user.location;
+        } else if (user.description) {
+            return user.description;
+        } else {
+            return null;
+        }
+    }
+
     render() {
         const { user } = this.props;
+
         if (user) {
             return (
-                <div className="user-profile-box">
-                    <div className="user-profile">
-                    
+                <div className="user-profile-buffer">
+                    <div className="user-profile-box">
+                        <div className="user-profile">
+                            <nav className="profile-nav">
+                                
+                            </nav>
+                            <section className="profile-body">
+                                <h2 className="profile-name">
+                                    {this.displayName()}
+                                </h2>
+                                <div className="profile-follows"></div>
+                                <div className="profile-description">
+                                    {this.displayDescription()}
+                                </div>
+                            </section>
+                            <nav className="profile-buttons">
+                                <button>Boards</button>
+                                <button>Pins</button>
+                            </nav>
+                        </div>
+                        <div className="profile-image-box">
+                            <div className="profile-image-container">
+                            </div>
+                        </div>
                     </div>
-                    <div className="user-image"></div>
-                    {user.username}
                 </div>
             )
         } else {
