5ba398123f361f6a4991443a4a728dec3e26bebb
Stephanie <stephani.kell@gmail.com>
2019-03-29 17:05:35 -0400

    Work on dynamic splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -4,37 +4,43 @@ import { Route, Redirect, Switch, Link, HashRouter, withRouter } from 'react-rou
 class Splash extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { pictures: [
-      "https://s3.amazonaws.com/pintrigue-pro/behappy.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/bells.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/blossoms.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/breakfast.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/bridge.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/cherries.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/citrus.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/city.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/coffee.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/door.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/leaves.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/letterpress.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/light_bulbs.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/map.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/mural.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/pencils.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/salad.jpg",
-      "https://s3.amazonaws.com/pintrigue-pro/watercolor.jpg"
-    ] };
-  }
-
-  componentDidMount() {
-
+    this.state = { 
+      pictures: [
+        "https://s3.amazonaws.com/pintrigue-pro/behappy.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/bells.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/blossoms.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/breakfast.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/bridge.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/cherries.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/citrus.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/city.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/coffee.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/door.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/leaves.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/letterpress.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/light_bulbs.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/map.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/mural.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/pencils.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/salad.jpg",
+        "https://s3.amazonaws.com/pintrigue-pro/watercolor.jpg"
+      ]
+    };
   }
 
   render() {
-    const pictures = this.state.pictures.map((url, i) => <img src={url} key={i}></img>)
+    const pictures = this.state.pictures.map((url, i) => {
+      return (
+        <div className="splash-pic-box" key={i} >
+          <img className="splash-pic" src={url} />
+        </div>
+      )
+    });
     return (
-      <div>
-        {pictures}
+      <div className="main-splash">
+        <div className="splash-container">
+          {pictures}
+        </div>
       </div>
     )
   }
